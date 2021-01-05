eb9543e62ffe83a3b8d5c9a4954704aa4f483bd8
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-01-05 13:33:23 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,32 +1,47 @@
-class Paper {
-   constructor(x, y,r) {
-      var options={
-         isStatic : false,
-         restitution: 0.3,
-         friction : 0.5,
-         density : 1.2,
-      }
 
-       this.body = Bodies.circle(x, y, r/2, options);
-       this.x = x;
-       this.y = y;
-       this.r = r;
-       
-       World.add(world, this.body);
-   }
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world, paper, ground, BottomBox, LeftBox, RightBox;
+
+function setup() {
+	createCanvas(800, 700);
+    background (0);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(50, 640, 10);
+	ground = new Ground(width/2, 670, width, 10);
+	
+	BottomBox = new Dustbin (600, 650, 200, 90);
+	LeftBox = new Dustbin (500, 610, 100, 0);
+	RightBox = new Dustbin (700, 610, 100, 0);
+
+	Engine.run(engine);
+  
+}
 
 
-   display() {
-   
-   var pos = this.body.position;
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
 
-   push();
-   translate(pos.x, pos.y)
-   ellipseMode(CENTER);
-   fill ("red")
-   stroke('red');
-   ellipse(0, 0, this.width, this.height);
-   pop();
-   }
+  paper.display();
+  ground.display();
+  BottomBox.display();
+  LeftBox.display();
+  RightBox.display();
+  
+  drawSprites();
+ 
+}
 
-   }
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+      Matter.Body.applyForce(paper.body, paper.body.position, {x:85, y:85})
+  }
+}
